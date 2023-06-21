8a7fc5810203c309a6ce2503c6dc3f8222007c10
Nico <juareznico61@gmail.com>
2023-06-21 17:15:03 -0300

    pagina ciro update

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -490,8 +490,6 @@ if (typeof jQuery === 'undefined') {
 +function ($) {
   'use strict';
 
-  // COLLAPSE PUBLIC CLASS DEFINITION
-  // ================================
 
   var Collapse = function (element, options) {
     this.$element      = $(element)
