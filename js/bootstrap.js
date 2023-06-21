5f1d6b04a9b8c0661d7b9e5655082bd249752f68
Nico <juareznico61@gmail.com>
2023-06-21 17:16:34 -0300

    pagina ciro update

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2015,13 +2015,6 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-/* ========================================================================
- * Bootstrap: affix.js v3.3.0
- * http://getbootstrap.com/javascript/#affix
- * ========================================================================
- * Copyright 2011-2014 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
 
 
 +function ($) {
