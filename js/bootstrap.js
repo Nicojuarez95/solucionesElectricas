f6be8b4ee8f521d478fcf90f88f3697c7b9bc6ba
Nico <juareznico61@gmail.com>
2023-06-21 17:16:18 -0300

    pagina ciro update

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1868,13 +1868,6 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-/* ========================================================================
- * Bootstrap: tab.js v3.3.0
- * http://getbootstrap.com/javascript/#tabs
- * ========================================================================
- * Copyright 2011-2014 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
 
 
 +function ($) {
