748ffee4766de82172004fe1c3e88883c4f2d1be
Nico <juareznico61@gmail.com>
2023-06-21 17:14:31 -0300

    pagina ciro update

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -145,14 +145,6 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-/* ========================================================================
- * Bootstrap: button.js v3.3.0
- * http://getbootstrap.com/javascript/#buttons
- * ========================================================================
- * Copyright 2011-2014 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
-
 
 +function ($) {
   'use strict';
